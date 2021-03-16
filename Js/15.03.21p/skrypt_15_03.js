59933125426d1fe0db3e175d0f64fcd9d4069716
MichalF-lab <micha-lfrackowiak@wp.pl>
2021-03-16 18:53:18 +0100

    commit to main

--- Js/15.03.21p/skrypt_15_03.js
+++ Js/15.03.21p/skrypt_15_03.js
@@ -4,19 +4,19 @@ guzik[0].addEventListener("click",zmiana1);
 
 function zmiana1(){
     let menu = document.querySelector("ul");
-    
-    menubar.classList.toggle("zmiana");
+    menu.classList.toggle("zmiana");
 }
 
-guzik[1].addEventListener("dblclick", function(){
+guzik[1].addEventListener("click", function(){
     let lista = document.querySelectorAll("li");
     lista[1].style.backgroundColor = "yellow";
-    lista[4].style[backgroundColor-color] = "red";
+    lista[4].style.backgroundColor = "red";
 })
 
+
 guzik[2].addEventListener("click", ()=>{
-    let lista = document.querySelectorAll9("li");
-    for (zmiana_czcionki of lista){
-        zmianna_czcionki.classList.toggle("czcionka");
+    let lista = document.querySelectorAll("li");
+    for (zmienna of lista){
+        zmienna.classList.add("czcionka");
     } 
 })
