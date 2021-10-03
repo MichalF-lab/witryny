627f248404eec7211504700ca616fe34b789235e
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-10-03 22:08:54 +0200

    ?

---
+++ 4_klasa/zad3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <title>Filomteka</title>
+        <link rel="stylesheet" href="styl3.css"/>
+    </head>
+    <body>
+        
+    </body>
+</html>
