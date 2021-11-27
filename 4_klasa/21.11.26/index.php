c6b232929a3b7013cd722b8697dc21a657f25db3
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-11-27 18:31:51 +0100

    commit to master

---
+++ 4_klasa/21.11.26/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Odżywianie zwierząt</title>
+        <link rel="stylesheet" href="style4.css">
+        <script defer>
+
+        </script>
+    </head>
+    <body>
+        <header><h2>DRAPIEŻNIKI I INNE</h2></header>
+        <nav>
+            <h3>
+                Wybierz styl życia:
+            </h3>
+            <form method="POST">
+                <select>
+                    <option>Drapieżniki</option>
+                    <option>Roślinożerne</option>
+                    <option>Padlinożerne</option>
+                    <option>Wszystkożerne</option>
+                </select>
+            </form>
+        </nav>
+        <main>
+            <div>
+                <h3>Lista zwierząt</h3>
+                <!--skrypt 1-->
+            </div>
+            <div><!--skrypt 2--></div>
+            <div><img src="drapieznik.jpg"></div>
+        </main>
+        <footer>
+            <a href="pl.wikipedia.org">Poczytaj o zwierzętach na Wikipedii</a>
+        </footer>
+    </body>
+</html>
