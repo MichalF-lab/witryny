d1b27aa7511c43ec0e5f4dfb482e7c8bc0b8fdf5
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-11-27 21:30:04 +0100

    commit to master

--- 4_klasa/21.11.26/index.php
+++ 4_klasa/21.11.26/index.php
@@ -4,9 +4,7 @@
         <meta charset="utf-8">
         <title>Odżywianie zwierząt</title>
         <link rel="stylesheet" href="style4.css">
-        <script defer>
-
-        </script>
+        <script>function fun(){window.location.reload()}</script>
     </head>
     <body>
         <header><h2>DRAPIEŻNIKI I INNE</h2></header>
@@ -14,25 +12,57 @@
             <h3>
                 Wybierz styl życia:
             </h3>
-            <form method="POST">
-                <select>
-                    <option>Drapieżniki</option>
-                    <option>Roślinożerne</option>
-                    <option>Padlinożerne</option>
-                    <option>Wszystkożerne</option>
+            <form method="POST" action="index.php">
+                <select name="zycie">
+                    <option value=1 onclick=fun()>Drapieżniki</option>
+                    <option value=2 onclick=fun()>Roślinożerne</option>
+                    <option value=3 onclick=fun()>Padlinożerne</option>
+                    <option value=4 onclick=fun()>Wszystkożerne</option>
                 </select>
             </form>
         </nav>
         <main>
-            <div>
+            <div id="a">
                 <h3>Lista zwierząt</h3>
-                <!--skrypt 1-->
+                <div id="d">
+                    <!--skrypt 1-->
+                    <?php
+                        $connect = new mysqli("localhost","root","","baza");
+                        $sql = "SELECT zwierzeta.gatunek, odzywianie.rodzaj
+                        FROM `zwierzeta`
+                        JOIN `odzywianie` on odzywianie.id = zwierzeta.Odzywianie_id;";
+                        $result = $connect->query($sql);
+                        echo '<ui>';
+                        $iterator = 0;
+                        while($wiersz = $result->fetch_assoc())
+                        {
+                            foreach($wiersz as $wynik)
+                            {
+                                if($iterator%2==0) echo "<li>$wynik -> ";
+                                if($iterator%2==1) echo "$wynik </li>";
+                                $iterator++;
+                            }
+                        }
+                        echo '</ui>';
+                        $connect->close();
+                    ?>
+                </div>
             </div>
-            <div><!--skrypt 2--></div>
-            <div><img src="drapieznik.jpg"></div>
+            <div id="b">
+                <!--skrypt 2-->
+                <?php
+                    $connect = new mysqli("localhost","root","","baza");
+                    $sql = "SELECT 'rodzaj' FROM `odzywianie`
+                    WHERE id = "
+                    $connect->close();
+                ?>
+            </div>
+            <div id="c"><img src="drapieznik.jpg"></div>
         </main>
         <footer>
             <a href="pl.wikipedia.org">Poczytaj o zwierzętach na Wikipedii</a>
+            <div>autor: Michal Frackowiak</div>
         </footer>
     </body>
 </html>
+<!--<ol>$wiersz[zwierzeta.gatunek]<ol> echo $wiersz['zwierzeta.'.'gatunek']; -->
