b8fee8ffef57ff1d6d93b99e319124dcd2b229a0
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2021-12-05 15:17:44 +0100

    commit to master

--- 4_klasa/21.11.19/pliki7/sklep.php
+++ 4_klasa/21.11.19/pliki7/sklep.php
@@ -3,8 +3,43 @@
     <head>
         <meta charset="utf-8">
         <title>Hurtownia</title>
+        <link href="styl2.css" rel="stylesheet">
     </head>
     <body>
-        
+        <header>
+            <span><img src="zad1.png" alt="hurtowania komputerowa"></span>
+            <nav>
+                <ol>
+                    <li>sprzet
+                        <ul>
+                            <li>Procesory</li>
+                            <li>Pamięci RAM</li>
+                            <li>Monitory</li>
+                            <li>Obudowy</li>
+                            <li>Karty graficzne</li>
+                            <li>Dyski twarde</li>
+                        </ul>
+                    </li>
+                    <li>Oprogramowanie</li>
+                </ol>
+            </nav>
+            <div>
+                <h2>Hurtownia komputerowa</h2>
+                <form method="POST">
+                    <span>Wybierz kategorię sprzetu</span>
+                    <input type="number" name="liczba">
+                    <input type="submit" value="SPRAWDZ">
+                </form>
+            </div>
+        </header>
+        <main>
+            <h1>Podzespoły we wskazanej kategorii</h1>
+            <!--skrypt 1-->
+        </main>
+        <footer>
+            <span>Hurtownia działa od poniedziałku do soboty w godzinach 7<sup>00</sup> - 14<sup>00</sup></span>
+            <a href="bok@hurtownia.pl">Napisz do nas</a>
+            <span>Prtenrzy: <a href="http://intel.pl/" target="_blank">Intel</a> <a href="http://amd.pl/" target="_blank">AMD</a></span>
+        </footer>
     </body>
 </html>
